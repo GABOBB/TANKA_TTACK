6034dac1ac3483d8f52b7cbce0f184645934909f
Gabriel BB <gabbol2002@gmail.com>
2024-09-28 12:41:44 -0600

    agrega clase de grafo

---
+++ grafo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gbb on 28/09/24.
+//
+
+#include "grafo.h"
---
+++ grafo.h
@@ -0,0 +1,16 @@
+//
+// Created by gbb on 28/09/24.
+//
+
+#ifndef GRAFO_H
+#define GRAFO_H
+
+
+
+class grafo {
+
+};
+
+
+
+#endif //GRAFO_H

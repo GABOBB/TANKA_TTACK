bafd5959282e0e87e77c85451bf1ae275bc79f86
Gabriel BB <gabbol2002@gmail.com>
2024-10-05 12:17:16 -0600

    se genera el traductor de mapa a matriz de adyacencia

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include "Graph.h"
 
 int main(){
-    Graph mapa;
+    Graph mapa = Graph(4,4);
     mapa.print();
 
-    mapa.agregar_arista(1,2,1);
-    mapa.agregar_arista(1,1,3);
-    mapa.agregar_arista(1,6,9);
-    mapa.agregar_arista(1,9,4);
-    mapa.agregar_arista(1,4,3);
+   mapa.prueva();
 
     mapa.print();
     return 0;
 }
+/*
+ mapa.agregar_arista(1,2,1);
+    mapa.agregar_arista(1,1,3);
+    mapa.agregar_arista(1,6,9);
+    mapa.agregar_arista(1,9,4);
+    mapa.agregar_arista(1,4,3);
+*/

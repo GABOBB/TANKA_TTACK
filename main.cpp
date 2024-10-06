5b8b5d0cba5c5baa1ba0ad5a77b643d59f43c18f
TheKrigerenJp <joguerrero@estudiantec.cr>
2024-10-06 10:11:37 -0600

    Commit con la ventana, ya esta el contador, donde va el tablero, puse 3 fondos diferentes y un combobox para poder cambiarlos, el borde negro que esta alrededor del tablero representa el limite para los tanques que luego se verá si se cambia por una imagen o se deja así

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include "ventana3.h"
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.setWindowTitle("Tank_Attack");
+    window.show();
+
+
+    return app.exec();
 }
---
+++ ventana3.h
@@ -0,0 +1,39 @@
+//
+// Created by josepa on 06/10/24.
+//
+
+#ifndef VENTANA3_H
+#define VENTANA3_H
+
+#include <QWidget>
+#include <QComboBox>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPixmap>
+#include <QTime>
+#include <QVector>
+class MainWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+    private slots:
+        void actualizarContador();
+    void cambiarFondo(int index);
+    void crearBordeAlrededor();     // Función para crear el borde alrededor del tablero
+
+private:
+    QLabel *label1;
+    QLabel *label2;
+    QLabel *contadorLabel;
+    QTime tiempoRestante;
+    QLabel *bordeSuperior;          // QLabel para el borde superior
+    QLabel *bordeInferior;          // QLabel para el borde inferior
+    QLabel *bordeIzquierdo;         // QLabel para el borde izquierdo
+    QLabel *bordeDerecho;           // QLabel para el borde derecho
+
+
+};
+
+#endif //VENTANA3_H

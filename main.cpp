08e462b8234c1a5d05ab7a9caa7349f144866cf0
Gabriel BB <gabbol2002@gmail.com>
2024-10-20 19:54:41 -0600

    se arregla el dijkstra y quedan las imagenes puestas temporalmente

--- main.cpp
+++ main.cpp
@@ -2,33 +2,6 @@
 #include "ventana3.h"
 #include "Graph.h"
 
-int main(int argc, char *argv[]) {
-    //QApplication app(argc, argv);
-
-    //MainWindow window;
-    //window.setWindowTitle("Tank_Attack");
-    //window.show();
-    vector<vector<int>> M_mapa = {
-                            {1,1,1,1,1},
-                            {1,0,0,1,1},
-                            {1,1,0,1,1},
-                            {1,1,0,1,1},
-                            {1,1,0,1,1}};
-    Graph mapa = Graph(5,5);
-    mapa.print();
-    mapa.mapa_adyacencia(M_mapa);
-    mapa.print();
-    coords in = coords(4,0);
-    coords out = coords(4,4 );
-    //int in[2] ={1,4};
-    //int out[2]= {4,2};
-    cout<<mapa.Dijkstra(in,out)<<endl;
-
-    return 0;//app.exec();
-
-
-}
-
 /*
 
     mapa.adyacencia_mapa();
@@ -42,3 +15,28 @@ int main(int argc, char *argv[]) {
      \/
      /\
 */
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.setWindowTitle("Tank_Attack");
+    window.show();
+    vector<vector<int>> M_mapa = {
+        {1,1,1,1,1},
+        {1,0,0,1,1},
+        {1,1,0,1,1},
+        {1,1,0,1,1},
+        {1,1,0,1,1}};
+    Graph mapa = Graph(5,5);
+    mapa.print();
+    mapa.mapa_adyacencia(M_mapa);
+    mapa.print();
+    coords in = coords(4,0);
+    coords out = coords(4,4 );
+
+    cout<<mapa.Dijkstra(in,out)<<endl;
+
+    return app.exec();
+
+
+}
